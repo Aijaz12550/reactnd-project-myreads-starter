83ae5daaab5df950e95da7d823df18917c58c591
zeeshanhanif <zeeshanhanif@gmail.com>
2018-12-22 18:58:31 +0500

    refactor: Separate components of Shelf and book and Fetched data from API

---
+++ src/components/shelf.js
@@ -0,0 +1,23 @@
+import React from 'react'
+// import * as BooksAPI from './BooksAPI'
+import Book from './book'
+
+class Shelf extends React.Component {
+  
+  render() {
+    return (
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            { this.props.books.map(book => {
+                return <Book key={book.title} book={book} />
+            })}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
